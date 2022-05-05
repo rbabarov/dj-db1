2d22ed4897ca3dea54a872011693f91ff0ba89c1
Roman Babarov <rbabarov@gmail.com>
2022-05-05 21:31:53 +0500

    def show add

--- main/urls.py
+++ main/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from phones.views import index, import_csv, show_catalog, show_product
+from phones.views import index, show_catalog, show_product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('catalog/', show_catalog, name='catalog'),
     path('catalog/<slug:slug>/', show_product, name='phone'),
-    path('import/', import_csv)
+
 ]
--- phones/views.py
+++ phones/views.py
@@ -11,34 +11,40 @@ def index(request):
 def show_catalog(request):
     template = 'catalog.html'
     context = {}
+    context['response'] = []
+
+    for p in Phone.objects.all():
+        context['response'].append(p)
+
+    if request.GET.get('sort') == 'name':
+        context['response'] = Phone.objects.order_by('name')
+
+    elif request.GET.get('sort') == 'cheap':
+        context['response'] = Phone.objects.order_by('price')
+
+    elif request.GET.get('sort') == 'expensive':
+        context['response'] = Phone.objects.order_by('-price')
+
     return render(request, template, context)
+
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    for phone in Phone.objects.all():
+        if phone.slug == slug:
+            description = f'Some info about the current phone: {phone.name}'
+            context = {'name': phone.name,
+                       'price': phone.price,
+                       'description': description,
+                       'image': phone.image,
+                       'release_date': phone.release_date,
+                       'lte_exists': phone.lte_exists,
+                       'slug': phone.slug
+                       }
+
     return render(request, template, context)
 
-def import_csv(request):
-    template = 'base.html'
-    context = {}
 
-    with open(settings.PHONES, 'r') as csvfile:
-        phone_reader = csv.reader(csvfile, delimiter=';')
-        # пропускаем заголовок
-        next(phone_reader)
-
-        # TODO: Добавьте сохранение модели
-        for line in phone_reader:
-            new_phone = Phone(
-                name=line[1],
-                price=int(line[3]),
-                image=line[2],
-                release_date=line[4],
-                lte_exists=line[5],
-                slug=''
-            )
-            new_phone.get_slug()
-            new_phone.save()
-    return render(request, template, context)
 
